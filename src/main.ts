9a16654cc6250df228752bc80f8e706e38765c2a
asom <86374869+as0mdude@users.noreply.github.com>
2024-10-10 23:28:01 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -78,7 +78,8 @@ const updateCounter = () => {
 const button = document.createElement('img');
 
 // Set the PNG image source
-button.src = "https://as0mdude.github.io/cmpm-121-demo-1/assets/img.png"; // Replace with the actual path to your PNG
+
+button.src = new URL('./assets/img.png', import.meta.url).href;
 
 // Set the button's styling to make it big and position it at the top
 button.style.width = '300px';  // Adjust width as needed for size
